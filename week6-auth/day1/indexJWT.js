1bd471fb994c880042e27422dfdfe9e64af9d132
Kunal Agrawal <kunal.agr69@gmail.com>
2025-03-27 13:36:56 +0530

    added week6

--- week6-auth/day1/indexJWT.js
+++ week6-auth/day1/indexJWT.js
@@ -38,7 +38,7 @@ app.post("/signin", (req, res) => {
         const token = jwt.sign({
             username:user.username  //the thing u want to encode in the token we write it here
         },JWT_SECRET);
-        
+        //anyone can decode this token u can check this in jwt.io
         res.json({
             token:token
         })
@@ -55,7 +55,8 @@ app.post("/signin", (req, res) => {
 app.get("/me", (req, res) => {
     const token = req.header.token;
     const userDetails = jwt.verify(token,JWT_SECRET); //the user after encoding the token is stored in the user
-    
+    //const userDetails = jwt.decode(token); this also decodes the token but causes vulnerablity as we are not verifying that this is our token only using jwt_secret
+
     //since the found the username only as the token contains only username we will have to search the data base (in this case its in memeory array to find other info like pass,etc) using its username
     const user = users.find(user => user.username == userDetails.username);
     if(user){
---
+++ week6-auth/day2/index.js
@@ -0,0 +1,99 @@
+//adding a authetication middleware that verifies is the user is logged in or not if not then cut it 
+
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "hiyou";
+
+//for converting req data to json
+app.use(express.json());
+
+const users=[];
+
+function logger (req,res,next){
+    console.log(req.method + " - Request came !")
+    next();
+}
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/public/index.html");  //frontend part ko idhar se connect kr diya like jaise hi localhost:3000 search krenge backend server strt krne ke badd then woh yeh file ko return krega 
+})
+
+app.post("/signup",logger, (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    //so that people cant signup twice
+    if(users.find(u=>u.usename == username)){
+        res.json({
+            msg:"Cannot sign up twice"
+        })
+        return;
+    }
+    users.push( {
+        username,
+        password
+    })
+    res.json({
+        msg:"Sign up was succesfull"
+    })
+});
+
+app.post("/signin",logger, (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(user => user.username === username && user.password === password); 
+
+    if (user) {
+        const token = jwt.sign({
+            username:user.username  //the thing u want to encode in the token we write it here
+        },JWT_SECRET);
+        //anyone can decode this token u can check this in jwt.io
+        res.json({
+            token:token
+        })
+    } 
+    else {
+        res.status(403).json({
+            message: "Invalid username or password"
+        })
+    }
+});
+
+function auth(req,res,next){
+    const token = req.headers.token;
+    if(token){
+        jwt.verify(token,JWT_SECRET,(err,decoded) =>{
+            if(decoded){
+                req.username = decoded.username; //changes the req of /me
+                next();
+            }else{
+                res.status(401).json({    //401 -> req is not authenticat
+                    msg:"unauthorised"
+                })
+            }
+        });
+    }else{
+        res.status(401).json({
+            msg:"not logged in"
+        })
+    }
+}
+
+app.get("/me",logger,auth,(req,res) => {
+    const user = users.find(user => user.username == req.username);
+
+    if(user){
+        res.json({
+            username :user.username
+        });
+    }
+    else{
+        res.json({
+            msg:"Invalid token"
+        });
+    }
+})
+ 
+app.listen(3000);
+
