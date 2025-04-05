14922036f4d8bdbf2143c6c0da864e57590fe3cf
Kunal Agrawal <kunal.agr69@gmail.com>
2025-04-06 00:00:35 +0530

    fixed the vulnerablities using zod bcrypt and try catch

--- week8-courseSellingApp/routes/admin.js
+++ week8-courseSellingApp/routes/admin.js
@@ -4,91 +4,134 @@ const { adminModel, courseModel } = require("../db");         //also using diffe
 const jwt = require("jsonwebtoken");
 const { JWT_ADMIN_PASSWORD } = require("../config");
 const { adminMiddleware } = require("../middleware/admin");
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
 
+adminRouter.post("/signup", async (req, res) => {
+    try {
+        const { email, password, firstname, lastname } = req.body;
+        //bcrypting password
+        const hashedpassword = await bcrypt.hash(password, 5);
 
-adminRouter.post("/signup", async (req,res) =>{
-    const { email, password, firstname, lastname } = req.body;
+        //stricting the schema..
+        const correctBody = z.object({
+            email: z.string().min(5).max(100).email(),
+            password: z.string().min(5).max(100).regex(/[A-z]/).regex(/[a-z]/).regex(/[0-9]/),           
+            firstname: z.string().min(5).max(100),
+            lastname: z.string().min(5).max(100)
+        })
+        const parseData = correctBody.safeParse(req.body);
+        if (!parseData.success) {
+            res.status(403).json({
+                msg: "Not correct schema",
+                error: parseData.error
+            });
+        }
 
-    await adminModel.create({
-        email:email,
-        password:password,
-        firstName:firstname,
-        lastName:lastname
-    });
+        await adminModel.create({
+            email: email,
+            password: hashedpassword,
+            firstName: firstname,
+            lastName: lastname
+        });
 
-    res.json({
-        msg:"Singup was succesfull"
-    })
-});                            
+        res.json({
+            msg: "Singup was succesfull"
+        })
+    } catch (e) {
+        res.status(400).json({
+            msg: "User already exist"
+        });
+    }
 
-adminRouter.post("/signin", async (req,res) =>{
+});
+
+adminRouter.post("/signin", async (req, res) => {
     const { email, password } = req.body;
     const response = await adminModel.findOne({
-        email: email,
-        password: password
+        email: email
     })
-    if(response){
+    //comparing pass with hasedpass
+    const matchedpassword = await bcrypt.compare(password, response.password);
+
+    if (response && matchedpassword) {
         const token = jwt.sign({
-            id:response._id
-        },JWT_ADMIN_PASSWORD);
+            id: response._id.toString()
+        }, JWT_ADMIN_PASSWORD);
         res.json({
-            token:token
+            token: token
         });
-    }else{
+    } else {
         res.status(403).json({
-            msg:"Incorrect credentials"
+            msg: "Incorrect credentials"
         });
     }
 });
 
 //try to fix this image url thing later
-adminRouter.post("/course",adminMiddleware, async (req,res) =>{     //to add course
+adminRouter.post("/course", adminMiddleware, async (req, res) => {     //to add course
     const adminId = req.userId;
-    const { title,descripttion,price,imageUrl } = req.body;
+    const { title, descripttion, price, imageUrl } = req.body;
+
+    //stricting the schema
+    const requiredBody = z.object({
+        title: z.string(),
+        description: z.string().min(3).max(100),
+        imageUrl: z.string().min(2),
+        price: z.number()
+    })
+    const parseData = requiredBody.safeParse(req.body);
+    if (!parseData.success) {
+        res.status(403).json({
+            msg: "Not correct schema",
+            error: parseData.error
+        });
+    }
 
     const course = await courseModel.create({
-        title:title,
-        description:descripttion,
-        price:price,
-        imageUrl:imageUrl,
-        creatorId:adminId
+        title: title,
+        description: descripttion,
+        price: price,
+        imageUrl: imageUrl,
+        creatorId: adminId
     });
     res.json({                          //this will send the id of the course by which it is sotred in the db
-        msg:"course created",
-        courseId:course._id
+        msg: "course created",
+        courseId: course._id
     })
 });
 
-adminRouter.put("/course",adminMiddleware, async (req,res) =>{    //to edit a course
+adminRouter.put("/course", adminMiddleware, async (req, res) => {    //to edit a course
     const adminId = req.userId;
-    const { title,descripttion,price,imageUrl,courseId } = req.body;
+    const { title, descripttion, price, imageUrl, courseId } = req.body;
 
     const course = await courseModel.updateOne({        //the first arguments wants the course ou want to change and the second one is the changes
-        _id:courseId,                   //this 2 lines make sures that the course id that the admin sent is a course made by the same admin only 
-        creatorId:adminId               //is prevents one crettor accessing other creator's courses
-    }, {            
-        title:title,
-        description:descripttion,
-        price:price,
-        imageUrl:imageUrl,
+        _id: courseId,                   //this 2 lines make sures that the course id that the admin sent is a course made by the same admin only 
+        creatorId: adminId               //is prevents one crettor accessing other creator's courses
+    }, {
+        title: title,
+        description: descripttion,
+        price: price,
+        imageUrl: imageUrl,
     });
     res.json({
-        msg:"Updated a course succesfully",
-        courseId:course._id
+        msg: "Updated a course succesfully",
+        courseId: course._id
     });
+    //not adding zod validation here becuase if creator only wants to change few things in the course then he will not have to write the whole schema
 });
 
-adminRouter.get("/course/bulk",adminMiddleware,async (req,res) =>{  //give all the courses of the creator
+adminRouter.get("/course/bulk", adminMiddleware, async (req, res) => {  //give all the courses of the creator
     const adminId = req.userId;
     const courses = await courseModel.find({   //find the courses that are creted by the admin
-        creatorId:adminId
+        creatorId: adminId
     });
     res.json({
-        msg:"Your courses are..",
-        courses:courses
+        msg: "Your courses are..",
+        courses: courses
     })
 });
 
 module.exports = {
-    adminRouter:adminRouter
+    adminRouter: adminRouter
 };
--- week8-courseSellingApp/routes/user.js
+++ week8-courseSellingApp/routes/user.js
@@ -1,62 +1,88 @@
 const { Router } = require("express");    //or you can --->both does the same thing
-                                          //const express = require("express");
-                                          //const {Router} = express.Router();
+//const express = require("express");
+//const {Router} = express.Router();
 const { userModel, purchaseModel, courseModel } = require("../db");
 const userRouter = Router();
 const jwt = require("jsonwebtoken");
 const { JWT_USER_PASSWORD } = require("../config");
 const { userMiddleware } = require("../middleware/user");
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
 
-userRouter.post("/signup",async (req,res) => {
-    const {email,password,firstname,lastname} = req.body;
-    await userModel.create({
-        email:email,
-        password:password,
-        firstName:firstname,
-        lastName:lastname
-    });
-    res.json({
-        msg:"Signup was succesfull"
-    });
-});
+userRouter.post("/signup", async (req, res) => {
+    try {
+        const { email, password, firstname, lastname } = req.body;
+        const hashedpassword = await bcrypt.hash(password, 5);
 
-userRouter.post("/signin", async (req,res) => {
-    const {email,password} = req.body;
-    const response = await userModel.findOne({
-        email:email,
-        password:password
-    });
-    if(response){
-        const token = jwt.sign({
-            id:response._id
-        },JWT_USER_PASSWORD);
-        res.json({
-            token:token
+        //stricting the schema..
+        const correctBody = z.object({
+            email: z.string().min(5).max(100).email(),
+            password: z.string().min(5).max(100).regex(/[A-z]/).regex(/[a-z]/).regex(/[0-9]/),
+            firstname: z.string().min(5).max(100),
+            lastname: z.string().min(5).max(100)
+        })
+        const parseData = correctBody.safeParse(req.body);
+        if (!parseData.success) {
+            res.status(403).json({
+                msg: "Not correct schema",
+                error: parseData.error
+            });
+        }
+
+        await userModel.create({
+            email: email,
+            password: hashedpassword,
+            firstName: firstname,
+            lastName: lastname
         });
-    }else{
-        res.status(403).json({
-            msg:"Incorrect credentials"
+        res.json({
+            msg: "Signup was succesfull"
+        });
+    } catch (e) {
+        res.status(400).json({
+            msg: "User already exist"
         });
     }
 });
 
-userRouter.get("/purchases",userMiddleware, async (req,res) => {        //get all the purchases
+userRouter.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    const response = await userModel.findOne({
+        email: email
+    });
+    const matchedpassword = await bcrypt.compare(password, response.password);
+
+    if (response && matchedpassword) {
+        const token = jwt.sign({
+            id: response._id.toString()
+        }, JWT_USER_PASSWORD);
+        res.json({
+            token: token
+        });
+    } else {
+        res.status(403).json({
+            msg: "Incorrect credentials"
+        });
+    }
+});
+
+userRouter.get("/purchases", userMiddleware, async (req, res) => {        //get all the purchases
     const userId = req.userId;
     const purchases = await purchaseModel.find({
         userId
     });
     //this will only show the ids of the courses that the user bought to see the course detail do..
-    
+
     const coursesData = await courseModel.find({
         _id: { $in: purchases.map(x => x.courseId) }
     })
 
     res.json({
-        purchases:purchases,
-        coursesData:coursesData
+        purchases: purchases,
+        coursesData: coursesData
     })
 });
 
 module.exports = {
-    userRouter:userRouter
+    userRouter: userRouter
 };
